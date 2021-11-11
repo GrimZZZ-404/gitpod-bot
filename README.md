c740a4dffa2e56a252064e09510d35eb27fc5f27
GrimZZZ-404 <shivanshsahay01@outlook.com>
2021-11-11 21:11:49 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gitpod Support Bot
+
+This repository contains source code of Gitpod Support Bot.
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/grimzzz-404/gitpod-bot)
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fgrimzzz-404%2Fgitpod-bot&plugins=redis&envs=TOKEN%2CCHANNEL%2CAUTOMSG%2CREACT%2CROLE&TOKENDesc=Discord+Bot+Token&CHANNELDesc=Array+Of+Channels&AUTOMSGDesc=Message+that+bot+should+send+after+creating+thread.&REACTDesc=Reaction&ROLEDesc=Authorized+role+that+can+archive+any+thread&REACTDefault=%E2%9C%85&ROLEDefault=Admin)
---
+++ index.js
@@ -0,0 +1,154 @@
+const logger = require('pino')({ level: 'info', transport: { target: 'pino-pretty' }});
+
+const { Client,	Collection,	Intents,	MessageActionRow,	MessageButton } = require('discord.js');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
+});
+
+const redis = require("redis");
+const db = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+const token = process.env.TOKEN;
+const channels = process.env.CHANNEL;
+const automessage = process.env.AUTOMSG;
+const reaction = process.env.REACT
+const authrole = process.env.ROLE
+
+client.once('ready', () => {
+    logger.info(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async message => {
+    if (message.content == `<@!${client.user.id}> close`) {
+        db.get(message.channel.id, async function(err, reply) {
+            if (err) {
+                logger.error(err);
+            } else {
+                if (reply == message.author.id || message.member.roles.cache.some(role => role.name === authrole)) {
+                    try {
+                        const msg = await message.channel.fetchStarterMessage();
+                        await msg.react(reaction);
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                    try {
+                        await message.channel.setArchived(true);
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                    logger.info(`Archived thread: ${message.channel.name}`);
+                } else {
+                    await message.reply("Unauthorized");
+                }
+            }
+        });
+    }
+
+    if (channels.indexOf(message.channelId) > -1 && message.content) {
+        try {
+            const thread = await message.startThread({
+                name: `"❓- "${message.content.substring(0, 50)}`,
+                autoArchiveDuration: 1440,
+                reason: 'Thread automation'
+            });
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('archive')
+                    .setLabel('Archive 🔒')
+                    .setStyle('SECONDARY'),
+                );
+            db.set(thread.id, message.author.id, async function(err, reply) {
+                if (err) {
+                    logger.error(err);
+                }
+            });
+            await thread.send({
+                content: automessage,
+                components: [row]
+            })
+            logger.info(`Created thread: ${thread.name}`);
+        } catch (error) {
+            logger.error(error)
+        }
+    }
+});
+
+client.on('threadUpdate', async (thread, thread1) => {
+    if (thread.archived == true && thread1.archived == false) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('archive')
+                .setLabel('Archive 🔒')
+                .setStyle('SECONDARY'),
+            );
+        try {
+            const msg = await thread.fetchStarterMessage();
+            await msg.reactions.removeAll();
+        } catch (error) {
+            logger.error(error);
+        }
+        try {
+            const newthread = await thread1.fetch()
+            if (newthread.archived == false) {
+                await newthread.send({
+                    content: 'Thread has been unarchived.',
+                    components: [row]
+                });
+            }
+        } catch (error) {
+            logger.error(error);
+
+        }
+        logger.info(`Unarchived thread: ${thread.name}`);
+    }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.isButton()) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('archived')
+                .setLabel('Archived 🔒')
+                .setStyle('SECONDARY')
+                .setDisabled(true),
+            );
+        db.get(interaction.channel.id, async function(err, reply) {
+            if (err) {
+                logger.error(err);
+            } else {
+                if (reply == interaction.user.id || interaction.member.roles.cache.some(role => role.name === authrole)) {
+                    try {
+                        const msg = await interaction.channel.fetchStarterMessage();
+                        await msg.react(reaction);
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                    try {
+                        await interaction.update({
+                            components: [row]
+                        });
+                        await interaction.channel.setArchived(true);
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                    logger.info(`Archived thread: ${interaction.channel.name}`);
+                } else {
+                    await interaction.reply({
+                        content: "Unauthorized",
+                        ephemeral: true
+                    });
+                }
+            }
+        });
+    }
+});
+
+db.once('ready', () => {
+    logger.info(`Database Connected`);
+    client.login(token);
+});
